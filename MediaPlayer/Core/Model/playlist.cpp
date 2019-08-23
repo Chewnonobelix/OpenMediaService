34470df5a799d9865ce667a39569c2a919d41b2f
Arnaud DUHAMEL <a.duhamel@ateme.com>
2019-08-23 16:17:29 +0200

    basic playlist

--- MediaPlayer/Core/Model/playlist.cpp
+++ MediaPlayer/Core/Model/playlist.cpp
@@ -2,5 +2,40 @@
 
 PlayList::PlayList()
 {
-    
+    setMetadata("id", QUuid::createUuid().toString());
+}
+
+QString PlayList::name() const
+{
+    return metaData<QString>("name");    
+}
+
+void PlayList::setName(QString name)
+{
+    setMetadata("name", name);
+}
+
+QString PlayList::id() const
+{
+    return metaData<QString>("id");
+}
+
+void PlayList::addMedia(MediaPointer m)
+{
+    m_playlist<<m;    
+}
+
+void PlayList::removeMedia(int index)
+{
+    m_playlist.removeAt(index);    
+}
+
+void PlayList::removeMedia(MediaPointer m)
+{
+    m_playlist.removeAll(m);    
+}
+
+QList<MediaPointer> PlayList::playlist() const
+{
+    return m_playlist;
 }
--- MediaPlayer/Core/Model/playlist.h
+++ MediaPlayer/Core/Model/playlist.h
@@ -1,17 +1,34 @@
 #ifndef PLAYLIST_H
 #define PLAYLIST_H
 
+#include <QList>
 #include <QSharedPointer>
+#include <QVariant>
+#include <QUuid>
 #include "global.h"
+#include "media.h"
 
 class PlayList;
 
 typedef QSharedPointer<PlayList> PlayListPointer;
 
-class PlayList
+class PlayList: public Metadata
 {
+private:
+    QList<MediaPointer> m_playlist;
+    
 public:
     PlayList();
+    
+    QString name() const;
+    void setName(QString);
+    
+    QString id() const;
+    
+    void addMedia(MediaPointer);
+    void removeMedia(int);
+    void removeMedia(MediaPointer);
+    QList<MediaPointer> playlist() const;
 };
 
 #endif // PLAYLIST_H
