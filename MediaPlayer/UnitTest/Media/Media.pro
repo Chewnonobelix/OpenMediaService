7a3e0b271154b0ca646692c2275b60db4fb1c049
Arnaud DUHAMEL <chewnonobelix@gmail.com>
2020-07-12 17:46:12 +0200

    [TU media] add first media tu

--- MediaPlayer/UnitTest/Media/Media.pro
+++ MediaPlayer/UnitTest/Media/Media.pro
@@ -8,3 +8,7 @@ TEMPLATE = app
 
 SOURCES +=  tst_mediatest.cpp
 
+INCLUDEPATH += ../../DesignLibrary/DesignPattern ../../ExpressionLibrary/LibExpression ../../Core/Model
+LIBS += -L$$OUT_PWD/../../DesignLibrary/DesignPattern/debug -lDesignPattern
+LIBS += -L$$OUT_PWD/../../ExpressionLibrary/LibExpression/debug -lLibExpression
+LIBS += -L$$OUT_PWD/../../Core/debug -lLibMediaPlayerCore
--- MediaPlayer/UnitTest/Media/tst_mediatest.cpp
+++ MediaPlayer/UnitTest/Media/tst_mediatest.cpp
@@ -1,10 +1,21 @@
 #include <QtTest>
-
+#include <QSignalSpy>
+#include "media.h"
 // add necessary includes here
 
 class MediaTest : public QObject
 {
     Q_OBJECT
+
+    Media model1, model2;
+    const MD5 id = "testid";
+    const QString path = "path1";
+    const int count = 1;
+    const MediaPlayerGlobal::MediaRole role = MediaPlayerGlobal::Game;
+    const QDate added = QDate::currentDate();
+    const QDateTime lastFinish = QDateTime::currentDateTime();
+    const double currentRead = 99.0;
+    const int rate = 2;
 
 public:
     MediaTest();
@@ -13,7 +24,16 @@ public:
 private slots:
     void initTestCase();
     void cleanupTestCase();
-    void test_case1();
+    void test_id();
+    void test_count();
+    void test_role();
+    void test_paths();
+    void test_isAvailable();
+    void test_removePath();
+    void test_added();
+    void test_lastFinish();
+    void test_currentRead();
+    void test_rating();
 
 };
 
@@ -37,10 +57,62 @@ void MediaTest::cleanupTestCase()
 
 }
 
-void MediaTest::test_case1()
+void MediaTest::test_id()
 {
-
+    QVERIFY(model1.id() != id);
+    model1.setId(id);
+    QCOMPARE(model1.id(), id);
 }
+
+void MediaTest::test_count()
+{
+    QVERIFY(model1.count() != count);
+    QSignalSpy spy(&model1, SIGNAL(countChanged(int)));
+    model1.setCount(count);
+    QCOMPARE(spy.count(), 1);
+    QCOMPARE(model1.count(), count);
+}
+
+void MediaTest::test_role()
+{
+    QFAIL("Not set");
+}
+
+void MediaTest::test_paths()
+{
+    QFAIL("Not set");
+}
+
+void MediaTest::test_isAvailable()
+{
+    QFAIL("Not set");
+}
+
+void MediaTest::test_removePath()
+{
+    QFAIL("Not set");
+}
+
+void MediaTest::test_added()
+{
+    QFAIL("Not set");
+}
+
+void MediaTest::test_lastFinish()
+{
+    QFAIL("Not set");
+}
+
+void MediaTest::test_currentRead()
+{
+    QFAIL("Not set");
+}
+
+void MediaTest::test_rating()
+{
+    QFAIL("Not set");
+}
+
 
 QTEST_APPLESS_MAIN(MediaTest)
 
