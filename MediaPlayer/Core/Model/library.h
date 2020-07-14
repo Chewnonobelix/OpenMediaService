7a126fa25a9be90b48ca7342882747ee3dbb85dd
Arnaud DUHAMEL <chewnonobelix@gmail.com>
2020-07-14 16:22:24 +0200

    clean library class

--- MediaPlayer/Core/Model/library.h
+++ MediaPlayer/Core/Model/library.h
@@ -7,7 +7,7 @@
 #include "media.h"
 #include "playlist.h"
 #include "libraryprobe.h"
-
+#include "metadata.h"
 
 using namespace MediaPlayerGlobal;
 
@@ -15,73 +15,42 @@ class Library;
 
 typedef QSharedPointer<Library> LibraryPointer;
 
-class Library: public QObject
+class Library: public QObject, public MetaData
 {
     Q_OBJECT
-    
-    
-    Q_PROPERTY(QString name READ name CONSTANT)
-    Q_PROPERTY(MediaPlayerGlobal::MediaRole role READ role CONSTANT)
-    Q_PROPERTY(QDateTime lastProbed READ lastProbed NOTIFY s_updateLastProbed)
-    Q_PROPERTY(QStringList sourceDir READ sourceDir NOTIFY s_updateSourceDirList)
-    Q_PROPERTY(int nbMedia READ nbMedia CONSTANT)
-    
-private:
-    QString m_name;
-    QSet<QString> m_sourceDir;
 
-    MediaPlayerGlobal::MediaRole m_role;    
-    QMap<MD5, MediaPointer> m_medias;    
-    QList<PlayListPointer> m_playlists;
-    QDateTime m_lastProbed;
-    
+    /**
+      Property:
+      -Medias
+      -Playlists
+      -Name
+      -Role
+      -Source dir
+      -last probed
+
+      methods:
+      -probe
+      -add manually (dir or file)
+
+      **/
+private:
     LibraryProbe m_probe;
 
-    bool in(MediaPointer) const;
-    
-    inline int nbMedia() const {return m_medias.size();}
     
 public:
     Library();
     Library(const Library&);
-    ~Library();
+    ~Library() = default;
 
     Library& operator = (const Library&);
     
     Q_INVOKABLE void probe();
 
-    QString name() const;
-    void setName(QString name);
-    
-    MediaPlayerGlobal::MediaRole role() const;
-    void setRole(MediaPlayerGlobal::MediaRole role);
-    
-    QMap<MD5, MediaPointer> medias() const;
-    void setMedias(const QMap<MD5, MediaPointer> &medias);
-    
-    Q_INVOKABLE void addSourceDir(QString);
-    QList<QString> sourceDir() const;
-    Q_INVOKABLE void removeSourceDir(QString);
-    
-    QDateTime lastProbed() const;
-    void setLastProbed(QDateTime lastProbed);
-    
-    Q_INVOKABLE void searchMedia() const;
 
 public slots:
-    void addProbedMedia(MD5, QString);
-    void endProbe();
-    void removeMedia(MD5);
-    bool addMedia(QString);
     
 signals:
-    void s_addMedia(MediaPointer);
-    void s_updateMedia(MediaPointer);
-    void s_removeMedia(MediaPointer);
-    void s_updateLastProbed(QDateTime);
-    void s_updateSourceDir(QSet<QString>);
-    void s_updateSourceDirList(QList<QString>);
-};
 
+};
 Q_DECLARE_METATYPE(Library)
 #endif // LIBRARY_H
