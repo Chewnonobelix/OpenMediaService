0c9e15fbb942dcf80fd40f51ee1e1f88eca9b30d
Arnaud DUHAMEL <a.duhamel@ateme.com>
2020-07-23 11:06:10 +0200

    [Library] arch

--- MediaPlayer/Core/Model/library.cpp
+++ MediaPlayer/Core/Model/library.cpp
@@ -1,1 +1,69 @@
 #include "library.h"
+
+Library::Library(const Library&)
+{}
+
+Library& Library::operator = (const Library&)
+{
+    return *this;
+}
+
+void Library::probe() 
+{}
+
+QUuid Library::id() const 
+{
+    return QUuid();
+}
+
+void Library::setId(QUuid)
+{}
+
+QString Library::name() const
+{
+    return QString();
+}
+
+void Library::setName(QString)
+{
+    
+}
+
+MediaPlayerGlobal::MediaRole Library::role() const
+{
+    return MediaPlayerGlobal::MediaRole::Undefined;    
+}
+
+void Library::setRole(MediaPlayerGlobal::MediaRole)
+{}
+
+bool Library::isShared() const
+{
+    return false;
+}
+
+void Library::setShared(bool)
+{}
+
+QDateTime Library::lastProbed() const
+{
+    return QDateTime();    
+}
+
+void Library::setLastProbed(QDateTime)
+{}
+
+QSet<QString> Library::sourceDir() const
+{
+    return QSet<QString>();
+}
+
+bool Library::addSourceDir(QString)
+{
+    return false;
+}
+
+bool Library::removeSourceDir(QString)
+{
+    return false;
+}
--- MediaPlayer/Core/Model/library.h
+++ MediaPlayer/Core/Model/library.h
@@ -33,12 +33,20 @@ class Library: public QObject, public MetaData
       -add manually (dir or file)
 
       **/
+    
+    Q_PROPERTY(QUuid id READ id CONSTANT)
+    Q_PROPERTY(QString name READ name WRITE setName NOTIFY nameChanged)
+    Q_PROPERTY(MediaPlayerGlobal::MediaRole role READ role CONSTANT)
+    Q_PROPERTY(bool isShared READ isShared WRITE setShared NOTIFY isSharedChanged)
+    Q_PROPERTY(QDateTime lastProbed READ lastProbed WRITE setLastProbed NOTIFY lastProbedChanged)
+    Q_PROPERTY(QSet<QString> sourceDir READ sourceDir NOTIFY sourceDirChanged CONSTANT)
+    
 private:
     LibraryProbe m_probe;
 
     
 public:
-    Library();
+    Library() = default;
     Library(const Library&);
     ~Library() = default;
 
@@ -47,10 +55,27 @@ public:
     Q_INVOKABLE void probe();
 
 
-public slots:
+    QUuid id() const;
+    void setId(QUuid);
+    QString name() const;
+    void setName(QString);
+    MediaPlayerGlobal::MediaRole role() const;
+    void setRole(MediaPlayerGlobal::MediaRole);
+    bool isShared() const;
+    void setShared(bool);
+    QDateTime lastProbed() const;
+    void setLastProbed(QDateTime);
+    QSet<QString> sourceDir() const;
+    Q_INVOKABLE bool addSourceDir(QString);
+    Q_INVOKABLE bool removeSourceDir(QString);
     
+public slots:
+     
 signals:
-
+    void nameChanged(QString);
+    void isSharedChanged(bool);
+    void lastProbedChanged(QDateTime);
+    void sourceDirChanged(QSet<QString>);
 };
 Q_DECLARE_METATYPE(Library)
 #endif // LIBRARY_H
