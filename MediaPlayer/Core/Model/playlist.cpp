fc10ba45e83d7f0e0e9ac110dc35353b32e9c76b
Arnaud DUHAMEL <chewnonobelix@gmail.com>
2021-01-21 17:49:54 +0100

    playlist play system

--- MediaPlayer/Core/Model/playlist.cpp
+++ MediaPlayer/Core/Model/playlist.cpp
@@ -1,70 +1,88 @@
 #include "playlist.h"
 
-PlayList::PlayList(const PlayList & p): QObject(nullptr), MetaData(p), QList(p)
-{}
+PlayList::PlayList(const PlayList &p)
+		: QObject(nullptr), MetaData(p), QList(p) {}
 
-PlayList& PlayList::operator= (const PlayList& p)
-{
-    MetaData& m = *this;
-    m = p;
-    QList<MediaPointer>& m2 = *this;
-    m2 = p;
-    
-    return *this;
+PlayList &PlayList::operator=(const PlayList &p) {
+	MetaData &m = *this;
+	m = p;
+	QList<MediaPointer> &m2 = *this;
+	m2 = p;
+
+	return *this;
 }
 
-QUuid PlayList::id() const
-{
-    return metaData<QUuid>("id");
+QUuid PlayList::id() const { return metaData<QUuid>("id"); }
+
+void PlayList::setId(QUuid id) { setMetadata("id", id); }
+
+QString PlayList::name() const { return metaData<QString>("name"); }
+
+void PlayList::setName(QString name) {
+	setMetadata("name", name);
+	emit nameChanged();
 }
 
-void PlayList::setId(QUuid id)
-{
-    setMetadata("id", id);
+bool PlayList::isShuffle() const { return metaData<bool>("isShuffle"); }
+
+void PlayList::setShuffle(bool s) {
+	setMetadata("isShuffle", s);
+	emit isShuffleChanged();
 }
 
-QString PlayList::name() const
-{
-    return metaData<QString>("name");
+void PlayList::append(MediaPointer m, int index) {
+	if (index == -1)
+		QList<MediaPointer>::append(m);
+	else
+		insert(index, m);
+
+	emit countChanged();
 }
 
-void PlayList::setName(QString name)
-{
-    setMetadata("name", name);
-    emit nameChanged(name);
+void PlayList::remove(int index) {
+	removeAt(index);
+
+	emit countChanged();
 }
 
-void PlayList::append(MediaPointer m, int index)
-{
-    if(index == -1)
-        QList<MediaPointer>::append(m);
-    else
-        insert(index, m);
-    
-    emit countChanged();
+MediaPointer PlayList::at(int i) const { return (*this)[i]; }
+
+void PlayList::swap(int i, int j) {
+	swapItemsAt(i, j);
+
+	emit countChanged();
 }
 
-void PlayList::remove(int index)
-{
-    removeAt(index);
-    
-    emit countChanged();
+void PlayList::clear() {
+	QList<MediaPointer>::clear();
+	emit countChanged();
 }
 
-MediaPointer PlayList::at(int i) const
-{
-    return (*this)[i];
+void PlayList::play(int index) {
+	if (isShuffle()) {
+		QSet<int> temp;
+		QRandomGenerator random;
+
+		while (temp.count() != count()) {
+			int it = random.bounded(0, count());
+			if (temp.contains(it))
+				continue;
+			temp << it;
+			m_readOrder.enqueue(it);
+		}
+	} else {
+		for (auto it = index; it < count(); it++) {
+			m_readOrder.enqueue(it);
+		}
+	}
 }
 
-void PlayList::swap(int i, int j)
-{
-    swapItemsAt(i, j);
-    
-    emit countChanged();
-}
-
-void PlayList::clear()
-{
-    QList<MediaPointer>::clear();
-    emit countChanged();
+MediaPointer PlayList::next() {
+	MediaPointer ret;
+	if (!m_readOrder.isEmpty()) {
+		auto index = m_readOrder.dequeue();
+		ret = at(index);
+	}
+	emit play(ret);
+	return ret;
 }
--- MediaPlayer/Core/Model/playlist.h
+++ MediaPlayer/Core/Model/playlist.h
@@ -1,6 +1,8 @@
 #pragma once
 
 #include <QList>
+#include <QQueue>
+#include <QRandomGenerator>
 #include <QSharedPointer>
 #include <QUuid>
 #include <QVariant>
@@ -22,8 +24,12 @@ class MEDIAPLAYERCORE_EXPORT PlayList : public QObject,
 	Q_PROPERTY(QUuid id READ id CONSTANT)
 	Q_PROPERTY(QString name READ name WRITE setName NOTIFY nameChanged)
 	Q_PROPERTY(int count READ count NOTIFY countChanged)
+	Q_PROPERTY(
+			bool isShuffle READ isShuffle WRITE setShuffle NOTIFY isShuffleChanged)
 
 private:
+	QQueue<int> m_readOrder;
+
 public:
 	PlayList() = default;
 	using MetaData::MetaData;
@@ -40,10 +46,17 @@ public:
 	void setId(QUuid);
 	QString name() const;
 	void setName(QString);
+	bool isShuffle() const;
+	void setShuffle(bool);
+
+	Q_INVOKABLE void play(int = 0);
+	Q_INVOKABLE MediaPointer next();
 
 signals:
-	void nameChanged(QString);
+	void nameChanged();
 	void countChanged();
+	void isShuffleChanged();
+	void play(MediaPointer);
 
 public slots:
 	Q_INVOKABLE virtual void append(MediaPointer, int = -1);
