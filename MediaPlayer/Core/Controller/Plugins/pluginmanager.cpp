7c570b3b345fb73925a6549842d61023e064d916
Perso <arnaud.duhamel.pro@gmail.com>
2021-04-26 17:33:35 +0200

    [Plugins] transform manager to model

--- MediaPlayer/Core/Controller/Plugins/pluginmanager.cpp
+++ MediaPlayer/Core/Controller/Plugins/pluginmanager.cpp
@@ -17,5 +17,27 @@ void PluginManager::init() {
 
 QSharedPointer<InterfacePlugins>
 PluginManager::operator[](MediaRole role) const {
-	return m_plugins[role];
+    if(m_plugins.contains(role))
+        return m_plugins[role];
+
+    return QSharedPointer<InterfacePlugins>();
+}
+
+QVariant PluginManager::data(const QModelIndex & index , int role) const
+{
+
+    return QVariant();
+}
+
+QHash<int, QByteArray> PluginManager::roleNames() const
+{
+    static QHash<int, QByteArray> ret = {{int(PluginRole::NameRole), "name"},
+                                         {int(PluginRole::EnableRole), "enable"}};
+
+    return ret;
+}
+
+int PluginManager::rowCount(QModelIndex const&) const
+{
+    return m_plugins.size();
 }
--- MediaPlayer/Core/Controller/Plugins/pluginmanager.h
+++ MediaPlayer/Core/Controller/Plugins/pluginmanager.h
@@ -2,20 +2,30 @@
 
 #include <QMap>
 #include <QPluginLoader>
+#include <QAbstractListModel>
 
 #include "interfaceplugins.h"
 #include <Model/global.h>
 
-class PluginManager {
+class PluginManager: public QAbstractListModel
+{
+    Q_OBJECT
+    Q_DISABLE_COPY(PluginManager)
+
 private:
+    enum class PluginRole {NameRole = Qt::DisplayRole +1, EnableRole};
+
 	QMap<MediaRole, QSharedPointer<InterfacePlugins>> m_plugins;
 
 public:
 	PluginManager() = default;
-	PluginManager(const PluginManager &) = default;
 	~PluginManager() = default;
 
 	void init();
 
 	QSharedPointer<InterfacePlugins> operator[](MediaRole) const;
+
+    int rowCount(QModelIndex const& = QModelIndex()) const override;
+    QVariant data(const QModelIndex & , int = Qt::DisplayRole) const override;
+    QHash<int, QByteArray> roleNames() const override;
 };
