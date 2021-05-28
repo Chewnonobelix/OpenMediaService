492c69223a032a39ec03a8e0e924f6562ddb9625
Perso <arnaud.duhamel.pro@gmail.com>
2021-05-28 16:58:10 +0200

    [Wrapper] create manager

--- MediaPlayer/Core/Controller/Core/tabwrapper.cpp
+++ MediaPlayer/Core/Controller/Core/tabwrapper.cpp
@@ -2,10 +2,17 @@
 
 QUuid TabWrapper::create()
 {
-    return QUuid();
+    auto manager = new TabManager;
+    m_model[manager->id()] = manager;
+
+    connect(manager, &TabManager::clicked, [this](QUuid id) {
+        m_current = id;
+    });
+
+    return manager->id();
 }
 
-QPointer<TabManager> TabWrapper::get(QUuid)
+QPointer<TabManager> TabWrapper::get(QUuid id)
 {
-    return nullptr;
+    return m_model.contains(id) ? m_model[id] : nullptr;
 }
