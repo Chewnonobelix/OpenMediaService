4bcd3f1bd4f8089bbe71eee7201606058c29757d
Perso <arnaud.duhamel.pro@gmail.com>
2021-07-02 16:27:30 +0200

    [AbstractRule] add new method

--- MediaPlayer/Core/Model/Smart/abstractrule.h
+++ MediaPlayer/Core/Model/Smart/abstractrule.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <Core/expression.h>
+#include <designpattern.h>
 
 class AbstractRule
 {
@@ -8,5 +9,7 @@ public:
     enum class Op {Superior, Inferior, Equal, Not, InferiorEqual, SuperiorEqual, Limit, And, Or, List, RegExp, Contain, Start, End};
 
     virtual QSharedPointer<Expression<bool>> create() = 0;
+    virtual QSharedPointer<AbstractRule> clone() const = 0;
+    virtual QPartialOrdering compare(QSharedPointer<AbstractRule>) const = 0;
 };
 
