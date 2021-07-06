884735315508251b389c7a44dfa58c471cd21a62
Perso <arnaud.duhamel.pro@gmail.com>
2021-07-06 17:08:33 +0200

    [SmartModel] add flat function

--- MediaPlayer/Core/Controller/DataModel/smartmodel.cpp
+++ MediaPlayer/Core/Controller/DataModel/smartmodel.cpp
@@ -1,8 +1,13 @@
 #include "smartmodel.h"
 
 SmartModel::SmartModel(QObject *parent)
-    : QAbstractListModel(parent)
+    : QAbstractTableModel(parent)
 {
+}
+
+int SmartModel::columnCount(const QModelIndex&) const
+{
+    return m_depth;
 }
 
 int SmartModel::rowCount(const QModelIndex &) const
@@ -25,6 +30,8 @@ QVariant SmartModel::data(const QModelIndex &index, int role) const
         return !group.isNull() ? QVariant::fromValue(group->op()) : QVariant();
     case SmartRole::TypeRole:
         return group.isNull() ? "rule" : "group";
+    case SmartRole::FieldRole:
+        return QStringList{"f1", "f2"};
     }
 
     return QVariant();
@@ -47,7 +54,8 @@ void SmartModel::setModel(SmartGroup m)
 QHash<int, QByteArray> SmartModel::roleNames() const
 {
     static QHash<int, QByteArray> ret = {{int(SmartRole::OpRole), "op"},
-                                         {int(SmartRole::TypeRole), "type"}
+                                         {int(SmartRole::TypeRole), "type"},
+                                         {int(SmartRole::FieldRole), "field"},
 
                                         };
 
@@ -62,5 +70,25 @@ SmartGroup SmartModel::group() const
 void SmartModel::setGroup(SmartGroup g)
 {
     m_model = g;
+    m_flat = toFlat(m_model);
     emit groupChanged();
+
+    qDebug()<<"Set group"<<m_flat.count();
+}
+
+QList<SmartModel::Flat> SmartModel::toFlat(SmartGroup& g, int depth)
+{
+   m_depth = std::max(m_depth, depth);
+
+   QList<SmartModel::Flat> ret;
+   ret << Flat{depth-1, "group", QSharedPointer<SmartGroup>::create(g)};
+   for(auto i = 0; i < g.count(); i++) {
+       if(!(g)[i].dynamicCast<SmartRule>().isNull()) {
+           ret << Flat{depth, "rule", (g)[i]};
+       }
+       else {
+           ret << toFlat(*(g)[i].dynamicCast<SmartGroup>(), depth+1);
+       }
+   }
+   return ret;
 }
--- MediaPlayer/Core/Controller/DataModel/smartmodel.h
+++ MediaPlayer/Core/Controller/DataModel/smartmodel.h
@@ -4,19 +4,29 @@
 
 #include <Model/Smart/smartgroup.h>
 
-class SmartModel : public QAbstractListModel
+class SmartModel : public QAbstractTableModel
 {
     Q_OBJECT
-    enum class SmartRole {OpRole = Qt::UserRole + 1, TypeRole};
+    enum class SmartRole {OpRole = Qt::UserRole + 1, TypeRole, FieldRole};
     Q_PROPERTY(SmartGroup group READ group WRITE setGroup NOTIFY groupChanged)
+
+    struct Flat {
+      int depth = 0;
+      QString type;
+      QSharedPointer<AbstractRule> rule;
+    };
 
 private:
     SmartGroup m_model;
+    QList<Flat> m_flat;
+    int m_depth = 0;
 
+    QList<Flat> toFlat(SmartGroup&, int = 1);
 public:
     explicit SmartModel(QObject *parent = nullptr);
 
     int rowCount(const QModelIndex & = QModelIndex()) const override;
+    int columnCount(const QModelIndex& = QModelIndex()) const override;
 
     QVariant data(const QModelIndex &, int = Qt::DisplayRole) const override;
 
