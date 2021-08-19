d62a2b86acc9b42722a60c8dc12af2459c1416ed
Perso <arnaud.duhamel.pro@gmail.com>
2021-08-19 12:25:14 +0200

    [SQL] add interface

--- OMS/Core/Controller/Data/datasql.cpp
+++ OMS/Core/Controller/Data/datasql.cpp
@@ -1,6 +1,24 @@
 #include "datasql.h"
 
-DataSql::DataSql()
-{
+DataSql::DataSql(const DataSql& data): InterfaceSaver(data)
+{}
 
+QMap<QUuid, LibraryPointer> DataSql::selectLibrary() const
+{
+    return QMap<QUuid, LibraryPointer>();
+}
+
+bool DataSql::createLibrary(QString, MediaPlayerGlobal::MediaRole)
+{
+    return false;
+}
+
+bool DataSql::removeLibrary(QString)
+{
+    return false;
+}
+
+bool DataSql::updateLibrary(LibraryPointer)
+{
+    return false;
 }
--- OMS/Core/Controller/Data/datasql.h
+++ OMS/Core/Controller/Data/datasql.h
@@ -1,9 +1,27 @@
 #pragma once
 
+#include <QSqlDatabase>
+#include <QSqlQuery>
 
-class DataSql
+#include <Controller/Data/interfacesaver.h>
+
+#include <Model/library.h>
+#include <Model/media.h>
+#include <Model/smartplaylist.h>
+
+class DataSql: public InterfaceSaver
 {
 public:
-    DataSql();
+    DataSql() = default;
+    DataSql(const DataSql &);
+    ~DataSql() override = default;
+
+    virtual void init() = 0;
+
+public slots:
+    QMap<QUuid, LibraryPointer> selectLibrary() const override;
+    bool createLibrary(QString, MediaPlayerGlobal::MediaRole) override;
+    bool removeLibrary(QString) override;
+    bool updateLibrary(LibraryPointer) override;
 };
 
