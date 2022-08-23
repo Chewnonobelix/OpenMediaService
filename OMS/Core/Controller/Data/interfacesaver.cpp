8c0ff72be485d7bca8a04e49fce9c81f838b2590
Arnaud DUHAMEL <arnaud.duhamel.pro@gmail.com>
2022-08-23 10:39:27 +0200

    [interface data save ] add virtual function

--- OMS/Core/Controller/Data/interfacesaver.cpp
+++ OMS/Core/Controller/Data/interfacesaver.cpp
@@ -1,3 +1,17 @@
 #include "interfacesaver.h"
 
 InterfaceSaver::InterfaceSaver(const InterfaceSaver &) : QObject() {}
+
+bool InterfaceSaver::insertMedia(MediaPointer) {return true;}
+bool InterfaceSaver::deleteMedia(QUuid) {return true;}
+bool InterfaceSaver::updateMedia(QUuid) {return true;}
+bool InterfaceSaver::insertPlaylist(PlaylistPointer) {return true;}
+bool InterfaceSaver::deletePlaylist(QUuid) {return true;}
+bool InterfaceSaver::updatePlaylist(QUuid) {return true;}
+bool InterfaceSaver::insertSmartGroup(SmartGroupPointer) {return true;}
+bool InterfaceSaver::deleteSmartGroup(QUuid) {return true;}
+bool InterfaceSaver::updateSmartGroup(QUuid) {return true;}
+bool InterfaceSaver::insertSmartRule(SmartRulePointer) {return true;}
+bool InterfaceSaver::deleteSmartRule(QUuid) {return true;}
+bool InterfaceSaver::updateSmartRule(QUuid) {return true;}
+
--- OMS/Core/Controller/Data/interfacesaver.h
+++ OMS/Core/Controller/Data/interfacesaver.h
@@ -22,6 +22,23 @@ public slots:
 	virtual bool removeLibrary(QString) = 0;
 	virtual bool updateLibrary(LibraryPointer) = 0;
 
+protected slots:
+    virtual bool insertMedia(MediaPointer);
+    virtual bool deleteMedia(QUuid);
+    virtual bool updateMedia(QUuid);
+
+    virtual bool insertPlaylist(PlaylistPointer);
+    virtual bool deletePlaylist(QUuid);
+    virtual bool updatePlaylist(QUuid);
+
+    virtual bool insertSmartGroup(SmartGroupPointer);
+    virtual bool deleteSmartGroup(QUuid);
+    virtual bool updateSmartGroup(QUuid);
+
+    virtual bool insertSmartRule(SmartRulePointer);
+    virtual bool deleteSmartRule(QUuid);
+    virtual bool updateSmartRule(QUuid);
+
 signals:
 	void librariesChanged();
 };
